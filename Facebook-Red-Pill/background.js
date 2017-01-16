262a704c4336bd142627b6f52d9b367802f9b6a6
Fede <federico.lochbaum@gmail.com>
2017-01-16 12:04:20 -0300

    algunos avances

--- Facebook-Red-Pill/background.js
+++ Facebook-Red-Pill/background.js
@@ -1,8 +1,25 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { //Colocamos la extension a la escucha de que una pagina Web sea visitada o actualizada.
 	
-	if(tab.url.indexOf(localStorage["urlExtension"]) != -1 && localStorage["urlExtension"] != ""){
-		alert("Estas visitando la pagina");
+	if(tab.url.indexOf("facebook") != -1){
+		alert("Estas visitando la pagina facebook");
+		startTimer()
 	}
+	else 
+		{
+			resetTimer()
+		}
 	
 });
 
+function startTimer(){
+	
+}
+
+function resetTimer(){
+
+}
+
+//chrome.tabs.onUpdated.addListener(function(tab, estado) { //Colocamos la extension a la escucha de que una pagina Web sea visitada o actualizada.
+//    if(estado.status == 'complete'){ //Si la pagina se ha terminado de cargar
+//            chrome.tabs.executeScript(tab.id, {'file':'timer.js'}); //Inyecta el código del fichero inyeccion.js
+//} 
